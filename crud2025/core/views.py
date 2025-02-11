8cb0163fba8bc1e2c632269835d8369761e36018
Wolfgrey00 <andoalpe@gmail.com>
2025-02-11 11:36:34 -0600

    Mi pagina

---
+++ crud2025/core/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render , HttpResponse
+# Create your views here.
+def home(request):
+    html_response = "<h1> mi pagina web</h1>"
+    for i in range(10):
+        html_response += "<p> Esto es la portada</p>"
+    return HttpResponse(html_response)
